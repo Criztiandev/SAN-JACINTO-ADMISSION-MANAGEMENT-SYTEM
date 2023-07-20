e329e495921030863f89d941bc6b4e60088e4783
criztiandev <criztiandev@gmail.com>
2023-07-21 06:39:06 +0800

    Added: Login Basic Logic

--- server/controller/auth.Controller.js
+++ server/controller/auth.Controller.js
@@ -1,6 +1,8 @@
 import asyncHandler from "express-async-handler";
 
-export const loginUser = asyncHandler(async (req, res) => {});
+export const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+});
 export const registerUser = asyncHandler(async (req, res) => {});
 export const forgotPassword = asyncHandler(async (req, res) => {});
 export const checkPoint = asyncHandler(async (req, res) => {});
--- server/routes/auth.Routes.js
+++ server/routes/auth.Routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { loginUser } from "../controller/auth.Controller";
+import { loginUser } from "../controller/auth.Controller.js";
 const router = express.Router();
 
 router.post("/login", loginUser);
