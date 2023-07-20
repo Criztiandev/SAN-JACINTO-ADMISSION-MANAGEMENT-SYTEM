6e1f74e9c95a4def04963a1754e20edc34fb5e17
criztiandev <criztiandev@gmail.com>
2023-07-21 07:06:41 +0800

    Added: Validation middleware to all routes

--- server/routes/auth.Routes.js
+++ server/routes/auth.Routes.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { loginUser, registerUser } from "../controller/auth.Controller.js";
+import { requestBodyValidation } from "../middleware/validation.js";
 const router = express.Router();
 
-router.post("/login", loginUser);
-router.post("/register", registerUser);
+router.post("/login", [requestBodyValidation], loginUser);
+router.post("/register", [requestBodyValidation], registerUser);
 
 export default router;
