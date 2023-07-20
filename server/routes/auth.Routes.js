bff0383c5025e944d6c9e2e28b6b897481b55e09
criztiandev <criztiandev@gmail.com>
2023-07-21 06:20:09 +0800

    Added: Initial Login Feature Setup

--- server/routes/auth.Routes.js
+++ server/routes/auth.Routes.js
@@ -1,4 +1,7 @@
 import express from "express";
+import { loginUser } from "../controller/auth.Controller";
 const router = express.Router();
+
+router.post("/login", loginUser);
 
 export default router;
