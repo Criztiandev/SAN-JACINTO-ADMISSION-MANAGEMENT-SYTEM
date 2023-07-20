8a29996209712088cd8ee0b098175e5ba25069b4
criztiandev <criztiandev@gmail.com>
2023-07-21 07:06:23 +0800

    Added: Request Validation that detect if there is no key or its an object as empty

--- server/middleware/validation.js
+++ server/middleware/validation.js
@@ -1,6 +1,9 @@
-export const requestBodyValidation = (req, res) => {
-  if (!req.body) {
+export const requestBodyValidation = (req, res, next) => {
+  console.log(req.body.length);
+  if (Object.keys(req.body).length === 0 || req.body.constructor === Object) {
     res.status(400);
     throw new Error("Invalid request body");
   }
+
+  next();
 };
