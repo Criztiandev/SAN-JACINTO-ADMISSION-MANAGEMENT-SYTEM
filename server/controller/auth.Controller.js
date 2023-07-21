2dec4e5683e6d14c3d23dd368da30a5b5cddd7db
criztiandev <criztiandev@gmail.com>
2023-07-21 08:37:42 +0800

    Re Added: Controller and Routes

--- server/controller/auth.Controller.js
+++ server/controller/auth.Controller.js
@@ -1,6 +1,61 @@
 import asyncHandler from "express-async-handler";
+import adminModel from "../models/adminModel.js";
+import { generateToken } from "../utils/token.utils.js";
 
-export const loginUser = asyncHandler(async (req, res) => {});
-export const registerUser = asyncHandler(async (req, res) => {});
+export const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await adminModel.findOne({
+    $or: [{ email: email }, { contact: email }],
+  });
+
+  if (!user) {
+    res.status(400);
+    throw new Error("Invalid email or contact, please try again");
+  }
+
+  if (!(await user.matchPassword(password))) {
+    res.status(400);
+    throw new Error("Invalid password, please try again");
+  }
+
+  // Generate token
+  generateToken(res, user._id);
+
+  res.status(200).json({
+    message: "User logged in successfully",
+  });
+});
+
+export const registerUser = asyncHandler(async (req, res) => {
+  const { email, contact } = req.body;
+
+  const emailExist = await adminModel.findOne({ email }).select("_id").lean();
+  if (emailExist) {
+    res.status(400);
+    throw new Error("Email already exists, please Try again");
+  }
+
+  const contactExist = await adminModel
+    .findOne({ contact })
+    .lean()
+    .select("_id");
+  if (contactExist) {
+    res.status(400);
+    throw new Error("Contact Already exist, please try again");
+  }
+
+  const newUser = await adminModel.create({ ...req.body });
+  if (!newUser) {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+
+  res.status(201).json({
+    _id: newUser._id,
+    email: newUser.email,
+    message: "User created successfully",
+  });
+});
 export const forgotPassword = asyncHandler(async (req, res) => {});
 export const checkPoint = asyncHandler(async (req, res) => {});
--- server/routes/auth.Routes.js
+++ server/routes/auth.Routes.js
@@ -1,7 +1,9 @@
 import express from "express";
-import { loginUser } from "../controller/auth.Controller";
+import { loginUser, registerUser } from "../controller/auth.Controller.js";
+import { requestBodyValidation } from "../middleware/validation.js";
 const router = express.Router();
 
-router.post("/login", loginUser);
+router.post("/login", [requestBodyValidation], loginUser);
+router.post("/register", [requestBodyValidation], registerUser);
 
 export default router;
