49be9eb82edfc399f6e061e1d5fa015958579220
criztiandev <criztiandev@gmail.com>
2023-07-21 08:15:51 +0800

    Fixed: Error Old commit

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import connectDb from "./config/connectDB.js";
+import { errorHandler, notFound } from "./middleware/errorHandler.js";
 
 // Configuration
 dotenv.config();
@@ -17,10 +18,8 @@ app.use(express.urlencoded({ extended: true })); // to accept form data
 app.use(cookieParser()); // to parse cookie data
 
 // Routes
-app.use("/api/auth", authRoutes);
 
-// Error Handlers
-app.use(customError);
+app.use(notFound);
 app.use(errorHandler);
 
 // Listeners
