e279a21d38e41074800cd40088d398d38983660e
criztiandev <criztiandev@gmail.com>
2023-07-21 08:41:22 +0800

    Updated: Server

--- server/server.js
+++ server/server.js
@@ -2,8 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import connectDb from "./config/connectDB.js";
-import { errorHandler, notFound } from "./middleware/errorHandler.js";
-
+import { notFound, errorHandler } from "./middleware/errorHandler.js";
+import { authRoutes } from "./routes/index.js";
 // Configuration
 dotenv.config();
 connectDb();
@@ -18,9 +18,10 @@ app.use(express.urlencoded({ extended: true })); // to accept form data
 app.use(cookieParser()); // to parse cookie data
 
 // Routes
+app.use("/api/auth", authRoutes);
 
+// Error Handlers
 app.use(notFound);
 app.use(errorHandler);
-
 // Listeners
 app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
