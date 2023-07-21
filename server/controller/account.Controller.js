57d046a543cf101ca559417c4fa8411e2bfaf9af
criztiandev <criztiandev@gmail.com>
2023-07-21 08:58:27 +0800

    Added: View Profile Credentials Basic Loginc

--- server/controller/account.Controller.js
+++ server/controller/account.Controller.js
@@ -1,5 +1,12 @@
 import asyncHandler from "express-async-handler";
 
-export const viewProfile = asyncHandler(async (req, res) => {});
+export const viewProfile = asyncHandler(async (req, res) => {
+  const UID = req.user;
+
+  res.status(200).json({
+    id: UID,
+    message: "Profile view",
+  });
+});
 export const updateProfile = asyncHandler(async (req, res) => {});
 export const deleteProfile = asyncHandler(async (req, res) => {});
