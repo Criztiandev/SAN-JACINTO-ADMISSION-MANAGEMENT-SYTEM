22a3be2f469c5aed9b19f130a4ed9b70010a1bce
criztiandev <criztiandev@gmail.com>
2023-07-21 09:01:54 +0800

    Added: Added Imporntant Middlewares

--- server/routes/accout.Routes.js
+++ server/routes/accout.Routes.js
@@ -1,10 +1,19 @@
 import express from "express";
 import { requestBodyValidation } from "../middleware/validation.js";
 import { viewProfile } from "../controller/account.Controller.js";
+import { protectedRoute } from "../middleware/protected.js";
 const router = express.Router();
 
-router.get("/profile", viewProfile);
-router.put("/profile/update", (req, res) => {});
-router.delete("/profile/delete", (req, res) => {});
+router.get("/profile", [protectedRoute], viewProfile);
+router.put(
+  "/profile/update",
+  [requestBodyValidation, protectedRoute],
+  (req, res) => {}
+);
+router.delete(
+  "/profile/delete",
+  [requestBodyValidation, protectedRoute],
+  (req, res) => {}
+);
 
 export default router;
