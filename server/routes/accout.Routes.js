82951d0306fa9f60c7ac8b24569da88a4bdd0e50
criztiandev <criztiandev@gmail.com>
2023-07-21 08:59:04 +0800

    Update and Add: Removed the unecessary import and add the Controller to the view profile route

--- server/routes/accout.Routes.js
+++ server/routes/accout.Routes.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { loginUser, registerUser } from "../controller/auth.Controller.js";
 import { requestBodyValidation } from "../middleware/validation.js";
+import { viewProfile } from "../controller/account.Controller.js";
 const router = express.Router();
 
-router.get("/profile", (req, res) => {});
+router.get("/profile", viewProfile);
 router.put("/profile/update", (req, res) => {});
 router.delete("/profile/delete", (req, res) => {});
 
