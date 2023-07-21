ddeb51c62859d2621f7bcc50603569004723f4dd
criztiandev <criztiandev@gmail.com>
2023-07-21 08:55:29 +0800

    Added: Initial Account Controller Setup

---
+++ server/controller/account.Controller.js
@@ -0,0 +1,5 @@
+import asyncHandler from "express-async-handler";
+
+export const viewProfile = asyncHandler(async (req, res) => {});
+export const updateProfile = asyncHandler(async (req, res) => {});
+export const deleteProfile = asyncHandler(async (req, res) => {});
