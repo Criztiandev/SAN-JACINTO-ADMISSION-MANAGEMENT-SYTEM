e333d459ac84b38fc65363c0327a2994b0c94c76
criztiandev <criztiandev@gmail.com>
2023-07-21 17:33:10 +0800

    Update: Trying to fix once again

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import connectDb from "./config/connectDB.js";
+import { errorHandler, notFound } from "./middleware/errorHandler.js";
+import { accountRoutes, authRoutes } from "./routes/index.js";
 
 // Configuration
 dotenv.config();
@@ -18,9 +20,10 @@ app.use(cookieParser()); // to parse cookie data
 
 // Routes
 app.use("/api/auth", authRoutes);
+app.use("/api/account", accountRoutes);
 
+// Error Handlers
 app.use(notFound);
 app.use(errorHandler);
-
 // Listeners
 app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
