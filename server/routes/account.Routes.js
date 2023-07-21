eeec40c228256e9cf834c99ccb9625782a3d2035
criztiandev <criztiandev@gmail.com>
2023-07-21 15:18:22 +0800

    Update: Instead of creating a single line for per route, we can use the route to make it one

--- server/routes/account.Routes.js
+++ server/routes/account.Routes.js
@@ -1,11 +1,17 @@
 import express from "express";
 import { validation } from "../middleware/validation.js";
-import { viewProfile } from "../controller/account.Controller.js";
+import {
+  deleteProfile,
+  updateProfile,
+  viewProfile,
+} from "../controller/account.Controller.js";
 import { protect } from "../middleware/protected.js";
 const router = express.Router();
 
-router.get("/profile", [protect], viewProfile);
-router.put("/profile/update", [validation, protect], (req, res) => {});
-router.delete("/profile/delete", [validation, protect], (req, res) => {});
+router
+  .route("/profile")
+  .get([protect], viewProfile)
+  .put([validation, protect], updateProfile)
+  .delete([validation, protect], deleteProfile);
 
 export default router;
