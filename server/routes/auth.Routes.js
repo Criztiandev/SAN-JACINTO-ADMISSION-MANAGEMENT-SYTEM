52e64fb8d09102bd10c78058475cb893f608fffb
criztiandev <criztiandev@gmail.com>
2023-07-22 08:03:58 +0800

    Created: reset and forgot password route

--- server/routes/auth.Routes.js
+++ server/routes/auth.Routes.js
@@ -1,9 +1,17 @@
 import express from "express";
-import { loginUser, registerUser } from "../controller/auth.Controller.js";
+import {
+  forgotPassword,
+  loginUser,
+  registerUser,
+  resetPassword,
+} from "../controller/auth.Controller.js";
 import { validation } from "../middleware/validation.js";
 const router = express.Router();
 
 router.post("/login", [validation], loginUser);
 router.post("/register", [validation], registerUser);
+
+router.post("/forgot-password", forgotPassword);
+router.get("/reset", resetPassword);
 
 export default router;
