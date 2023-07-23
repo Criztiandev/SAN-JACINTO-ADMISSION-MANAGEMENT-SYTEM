c947bbc7e3b3e13f2250cb694c6315d96429cecc
criztiandev <criztiandev@gmail.com>
2023-07-23 08:02:07 +0800

    Update: Added the verfication and protected route

--- server/routes/auth.Routes.js
+++ server/routes/auth.Routes.js
@@ -4,8 +4,10 @@ import {
   loginUser,
   registerUser,
   getResetToken,
+  verifyResetToken,
 } from "../controller/auth.Controller.js";
 import { validation } from "../middleware/validation.js";
+import { resetProt } from "../middleware/protected.js";
 const router = express.Router();
 
 router.post("/login", [validation], loginUser);
@@ -13,5 +15,6 @@ router.post("/register", [validation], registerUser);
 
 router.post("/forgot-password", forgotPassword);
 router.get("/checkpoint/:id/:token", getResetToken);
+router.post("/checkpoint/verify", [resetProt], verifyResetToken);
 
 export default router;
