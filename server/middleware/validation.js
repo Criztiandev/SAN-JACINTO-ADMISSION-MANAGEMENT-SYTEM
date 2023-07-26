93079473ffbe6fb2f60c17b8f1688a995114f4db
criztiandev <criztiandev@gmail.com>
2023-07-26 17:37:24 +0800

    Remove: name is not appropriate

--- server/middleware/validation.js
+++
@@ -1,7 +0,0 @@
-export const validation = (req, res, next) => {
-  if (Object.keys(req.body).length === 0) {
-    res.status(400);
-    throw new Error("Invalid request body");
-  }
-  return next();
-};
