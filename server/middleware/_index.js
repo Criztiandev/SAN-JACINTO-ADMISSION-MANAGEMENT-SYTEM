8c7bb990cb090012dd865050a290f7c67ebecd6d
criztiandev <criztiandev@gmail.com>
2023-07-26 18:02:27 +0800

    Imported session and user middleware

--- server/middleware/_index.js
+++ server/middleware/_index.js
@@ -1,3 +1,5 @@
 export { protect } from "./protected.Middleware.js";
 export { notFound, errorHandler } from "./error.Middleware.js";
 export { validation } from "./validation.Middleware.js";
+export { validateSession } from "./session.Middleware.js";
+export { validateUser } from "./user.Middleware.js";
