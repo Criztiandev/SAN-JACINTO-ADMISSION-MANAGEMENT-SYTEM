5c00fb4f7980d1e233f112679d17368cdc2a6a43
criztiandev <criztiandev@gmail.com>
2023-07-26 16:12:49 +0800

    Update: Removed the old logic and added the new as req.session.user for starting the session

--- server/middleware/protected.js
+++ server/middleware/protected.js
@@ -15,60 +15,10 @@ export const protect = asyncHandler(async (req, res, next) => {
   }
 
   try {
-    const decoded = verifyToken(token);
-
-    // if you want to dynamic validate its role
-
-    const user = await adminModel
-      .findById(decoded.UID)
-      .select("-password")
-      .lean();
-
-    if (!user) {
-      res.status(404);
-      throw new Error("User Not Found");
-    }
-
-    req.user = user;
-
-    if (!req.user) {
-      res.status(401);
-      throw new Error("Request not authorized");
-    }
-
+    req.session.user = validateTokenFromCookies(name, token);
     next();
   } catch (error) {
     res.status(401);
-    throw new Error("Invalid token, please login");
-  }
-});
-
-export const resetProt = asyncHandler(async (req, res, next) => {
-  let token = req.cookies.prch || null;
-
-  if (!token) {
-    res.status(401);
-    throw new Error("Unauthorized access, please login");
-  }
-
-  try {
-    const decoded = verifyMagicToken(token, process.env.MAGIC_SECRET);
-
-    if (!decoded) {
-      res.status(401);
-      throw new Error("Token not verfied, please try again");
-    }
-
-    req.prot = await adminModel.findById(decoded._id).select("_id").lean();
-    if (!req.prot) {
-      res.status(404);
-      throw new Error("User Not Found");
-    }
-
-    next();
-  } catch (e) {
-    res.status(401);
-    console.log(e);
     throw new Error("Invalid token, please login");
   }
 });
