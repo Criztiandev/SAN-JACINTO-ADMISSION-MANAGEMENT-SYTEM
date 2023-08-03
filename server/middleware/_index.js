347f9e1193ef832a0eb88315e7464b11e976329d
criztiandev <criztiandev@gmail.com>
2023-08-03 17:22:49 +0800

    Update: Change name to appropriate one

--- server/middleware/_index.js
+++ server/middleware/_index.js
@@ -2,4 +2,4 @@ export { protect } from "./protected.Middleware.js";
 export { notFound, errorHandler } from "./error.Middleware.js";
 export { validation } from "./validation.Middleware.js";
 export { validateSession } from "./session.Middleware.js";
-export { validateUser } from "./user.Middleware.js";
+export { validateAdmin } from "./user.Middleware.js";
