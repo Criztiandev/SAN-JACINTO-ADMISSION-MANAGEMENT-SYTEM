f48e758f056b6224564d3eba334c418ab421dda3
criztiandev <criztiandev@gmail.com>
2023-08-03 20:19:00 +0800

    Commeted temporary sendng email

--- server/controller/auth/forgotPass.feature.js
+++ server/controller/auth/forgotPass.feature.js
@@ -5,42 +5,39 @@ import { sendEmail } from "../../utils/email.uitls.js";
 import { encryptData } from "../../utils/encryption.utils.js";
 
 const forgotPassFeature = asyncHandler(async (req, res) => {
-  const { email } = req.body;
+  try {
+    const { email } = req.body;
 
-  // check if the user exist
-  const user = await adminModel
-    .findOne({ $or: [{ email: email }, { contact: email }] })
-    .lean()
-    .select("_id updatedAt");
-  if (!user) {
+    // check if the user exist
+    const user = await adminModel.findUser({ email }, { exist: true });
+
+    // create a one time password link that is valid for 15 minutes
+    const secret = generateSecret({ token: null, secret: user.updatedAt });
+    const token = generateToken(user._id, secret, "15m");
+
+    const encryptedID = encryptData(user._id.toString());
+    const link = `${process.env.FGT_BASE}${encryptedID}/${token}`;
+
+    // const content = {
+    //   target: email,
+    //   title: "Forgot Password Verfication",
+    //   body: link,
+    // };
+
+    // const mail = await sendEmail(res, content);
+    // if (!mail) {
+    //   res.status(400);
+    //   throw new Error("Email not sent, please try again");
+    // }
+
+    res.status(200).json({
+      link: link,
+      message: "Email sent successfully",
+    });
+  } catch (error) {
     res.status(400);
-    throw new Error("Invalid email or password");
+    throw new Error(error);
   }
-
-  // create a one time password link that is valid for 15 minutes
-  const secret = generateSecret({ token: null, secret: user.updatedAt });
-  console.log(secret);
-  const token = generateToken(user._id, secret, "15m");
-
-  const encryptedID = encryptData(user._id.toString());
-  const link = `${process.env.FGT_BASE}${encryptedID}/${token}`;
-
-  const content = {
-    target: email,
-    title: "Forgot Password Verfication",
-    body: link,
-  };
-
-  const mail = await sendEmail(res, content);
-  if (!mail) {
-    res.status(400);
-    throw new Error("Email not sent, please try again");
-  }
-
-  res.status(200).json({
-    link: link,
-    message: "Email sent successfully",
-  });
 });
 
 export default forgotPassFeature;
