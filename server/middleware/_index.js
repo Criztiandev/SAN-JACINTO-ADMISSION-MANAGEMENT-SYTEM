29d2e2cb25a433311169cc201e7fd3844156b490
criztiandev <criztiandev@gmail.com>
2023-08-04 09:30:31 +0800

    Update: Exporting

--- server/middleware/_index.js
+++ server/middleware/_index.js
@@ -1,6 +1,6 @@
-export { protect } from "./protected.Middleware.js";
-export { validateSession } from "./session.Middleware.js";
+export { protect } from "./private/protected.Middleware.js";
+export { validateSession } from "./private/session.Middleware.js";
 
-export { notFound, errorHandler } from "./error.middleware.js";
-export { validation } from "./validation.middleware.js";
-export { validateAdmin } from "./user.middleware.js";
+export { notFound, errorHandler } from "./development/error.middleware.js";
+export { validation } from "./validation/requestbody.middleware.js";
+export { validateAdmin } from "./validation/user.middleware.js";
