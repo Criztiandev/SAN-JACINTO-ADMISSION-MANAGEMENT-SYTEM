e2ea9f03cd30c38429451490a4223c8345d8dd88
criztiandev <criztiandev@gmail.com>
2023-08-04 17:15:57 +0800

    Added: Initial setup for feature

---
+++ server/controller/applicants/create.applicant.feature.js
@@ -0,0 +1,3 @@
+import asyncHandler from "express-async-handler";
+
+const createApplicant = asyncHandler(async (req, res) => {});
---
+++ server/controller/applicants/update.applicant.feature.js
@@ -0,0 +1,3 @@
+import asyncHandler from "express-async-handler";
+
+const updatelApplicant = asyncHandler(async (req, res) => {});
