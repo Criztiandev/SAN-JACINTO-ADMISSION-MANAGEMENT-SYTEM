39fd42a6e6980681f341a24af527f3182d3c4790
criztiandev <criztiandev@gmail.com>
2023-08-13 10:05:47 +0800

    Added Applicant feature

--- server/routes/applicant.routes.js
+++ server/routes/applicant.routes.js
@@ -1,4 +1,9 @@
 import express from "express";
+import createApplicant from "../controller/applicants/create.applicant.feature.js";
 const router = express.Router();
+
+router.post("/create", createApplicant);
+
+// get all applicants
 
 export default router;
