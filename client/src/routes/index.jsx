0fc75a17e44ebee2665d9cb35ad0dda99b7bff25
criztiandev <criztiandev@gmail.com>
2023-08-13 10:28:53 +0800

    Setup error page

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -1,9 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
+import ErrorPage from "../pages/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <h1>HI</h1>,
+    errorElement: <ErrorPage />,
   },
 ]);
 
