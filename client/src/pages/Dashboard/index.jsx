15d34f349e647c81df2445cd30f6185b8fb030e9
criztiandev <criztiandev@gmail.com>
2023-08-13 11:33:21 +0800

    Added Dashboarr setup

---
+++ client/src/pages/Dashboard/index.jsx
@@ -0,0 +1,11 @@
+import Sidebar from "../../components/Sidebar";
+
+const Dashboard = () => {
+  return (
+    <section className="grid grid-cols-dashboard-layout h-[100vh]">
+      <Sidebar />
+    </section>
+  );
+};
+
+export default Dashboard;
