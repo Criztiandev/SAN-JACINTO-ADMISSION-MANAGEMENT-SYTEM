bb0da1317b7edd0f74dd2d3a45c691ea96756d6c
criztiandev <criztiandev@gmail.com>
2023-08-29 05:54:21 +0800

    Created a Input Component

--- client/src/components/Input.tsx
+++ client/src/components/Input.tsx
@@ -1,8 +1,24 @@
 import styled from '@emotion/styled'
 import { useTheme } from '@emotion/react'
 
-const Input = () => {
-  return <div>Input</div>
+interface InputProps {
+  type: string
+  name: string
+}
+
+interface BaseInputProps {
+  theme: any
+}
+
+const BaseInput = styled.input<BaseInputProps>`
+  background: ${({ theme }) => theme.colors.primary};
+  border-radius: 5px;
+  padding: 4px;
+`
+
+const Input = ({ type, name }: InputProps) => {
+  const theme = useTheme()
+  return <BaseInput theme={theme} type='text' />
 }
 
 export default Input
