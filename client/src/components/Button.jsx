848fd44cbe6767ee11c018acb63f38862f0b6810
criztiandev <criztiandev@gmail.com>
2023-08-29 04:10:20 +0800

    Added Typescript Configuration

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -1,32 +1,68 @@
+import styled from "@emotion/styled";
 import PropTypes from "prop-types";
-import { useTheme } from "../context/ThemeContext";
 
-const Button = ({ className, variants, dir, icon, children, ...props }) => {
-  const { variant, tokens } = useTheme();
-  const { button } = tokens;
-  const base = `flex gap-[${button.gap}] px-[${button.horizontal}] py-[${button.vertical}]`;
+const BaseButton = styled.button`
+  background-color: ${({ theme }) => theme.colors.background};
+  color: ${({ theme }) => theme.colors.text};
+  padding: 0.5rem 1rem;
+  border-radius: 0.25rem;
+  border: 1px solid black;
+  font-size: 1rem;
+  font-weight: 500;
+`;
 
-  // direction element checker
+const RoundedButton = styled(BaseButton)`
+  border-radius: 1rem;
+`;
+
+const GhostButton = styled(BaseButton)`
+  background-color: transparent;
+  border: none;
+`;
+
+const SmallButton = styled(BaseButton)`
+  padding: 0.25rem 0.5rem;
+  font-size: 0.875rem;
+`;
+
+const LargeButton = styled(BaseButton)`
+  padding: 0.75rem 1.5rem;
+  font-size: 1.25rem;
+`;
+
+const Button = ({ variant, size, dir, icon, children }) => {
+  const VariantComponent = {
+    default: BaseButton,
+    rounded: RoundedButton,
+    ghost: GhostButton,
+  }[variant];
+
+  const SizeComponent = {
+    sm: SmallButton,
+    md: BaseButton,
+    lg: LargeButton,
+  }[size];
 
   return (
-    <button {...props} className={`${base} ${variant[variants]} ${className}`}>
-      {dir === "left" && icon ? <span>{icon}</span> : null}
+    <VariantComponent as={SizeComponent} dir={dir}>
+      {icon && <img className="w-[24px] h-[24px]" src={icon} alt="" />}
       {children}
-      {dir === "right" && icon ? <span>{icon}</span> : null}
-    </button>
+    </VariantComponent>
   );
 };
 
 Button.propTypes = {
   className: PropTypes.string,
-  variants: PropTypes.oneOf(["default", "rounded", "ghost"]).isRequired,
+  variant: PropTypes.oneOf(["default", "rounded", "ghost"]),
+  size: PropTypes.oneOf(["sm", "md", "lg"]),
   dir: PropTypes.oneOf(["right", "left"]).isRequired,
   icon: PropTypes.element,
   children: PropTypes.node.isRequired,
 };
 
 Button.defaultProps = {
-  variants: "default",
+  variant: "default",
+  size: "lg",
   dir: "left",
 };
 
--- client/styleTokens.js
+++ client/styleTokens.js
@@ -1,27 +1,8 @@
 export const styleTokens = {
-  typography: {
-    fontFamily: "Roboto, sans-serif",
-  },
   colors: {
     primary: "#1E90FF",
     secondary: "#FFA500",
     tertiary: "#FF6347",
     quaternary: "#FFD700",
-  },
-
-  border: {
-    primary: "#D9d9d9",
-  },
-
-  button: {
-    iconP: "4px",
-    horizontal: "24px",
-    vertical: "8px",
-    gap: "0.5px",
-  },
-
-  input: {
-    horizontal: "12px",
-    vertical: "8px",
   },
 };
