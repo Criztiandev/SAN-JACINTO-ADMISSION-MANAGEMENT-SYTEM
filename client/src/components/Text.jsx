8da902449bf3bc09aa7be118bc5ce654650b5099
criztiandev <criztiandev@gmail.com>
2023-08-28 16:10:16 +0800

    Created Radio Selection

--- client/src/components/Text.jsx
+++ client/src/components/Text.jsx
@@ -1,5 +1,9 @@
-const Text = () => {
-  return <div>Text</div>;
+import PropTypes from "prop-types";
+const Text = ({ content }) => {
+  return <p>{content}</p>;
 };
 
+Text.propTypes = {
+  content: PropTypes.string.isRequired,
+};
 export default Text;
