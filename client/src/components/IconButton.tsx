dbf9c7543c24f18f21b9eef5289cfcd2abf65415
criztiandev <criztiandev@gmail.com>
2023-08-29 05:37:44 +0800

    Added Icon Button Variants

--- client/src/components/IconButton.tsx
+++ client/src/components/IconButton.tsx
@@ -14,16 +14,38 @@ interface BaseIconProps {
   size?: number
 }
 
+interface IconButtonType {
+  default: typeof BaseIconButton
+  outlined: typeof Outlined
+  ghost: typeof Ghost
+}
+
 const BaseIconButton = styled.button<BaseIconProps>`
   background: ${({ theme }) => theme.colors.primary};
   border-radius: ${({ rounded }) => (rounded ? '24px' : '5px')};
   padding: ${({ size }) => (size ? `${size}px` : '4px')};
 `
 
+const Outlined = styled(BaseIconButton)`
+  background-color: transparent;
+  border: 1px solid ${({ theme }) => theme.colors.border};
+`
+
+const Ghost = styled(BaseIconButton)`
+  background-color: transparent;
+  border: none;
+`
+
 const IconButton = ({ variant = 'default', ...props }: IconButtonProps) => {
   const theme = useTheme()
+  const types: IconButtonType = {
+    default: BaseIconButton,
+    outlined: Outlined,
+    ghost: Ghost
+  }
 
-  const IconVariant = BaseIconButton
+  const IconVariant = types[variant] || BaseIconButton
+
   return (
     <IconVariant theme={theme} {...props}>
       <img
---
+++ client/src/components/Input.tsx
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled'
+import { useTheme } from '@emotion/react'
+
+const Input = () => {
+  return <div>Input</div>
+}
+
+export default Input
