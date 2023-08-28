1551d373ef2d6a5eb4dfe5b494295de1c4175901
criztiandev <criztiandev@gmail.com>
2023-08-28 10:35:34 +0800

    Update error on Theme Context

--- client/src/context/ThemeContext.jsx
+++ client/src/context/ThemeContext.jsx
@@ -1,24 +1,33 @@
 import PropTypes from "prop-types";
-import { createContext, useState } from "react";
+import { createContext, useContext, useState } from "react";
 
 const ThemeContext = createContext({});
 
 // eslint-disable-next-line react-refresh/only-export-components
-export const useTheme = createContext(ThemeContext);
-
-ThemeProvider.propTypes = {
-  tokens: PropTypes.object.isRequired,
-  children: PropTypes.node.isRequired,
-};
+export const useTheme = () => useContext(ThemeContext);
 
 const ThemeProvider = ({ tokens, children }) => {
   const [colors, setColors] = useState(tokens.colors);
   const [fonts, setFonts] = useState(tokens.typograhpy);
+
+  const updateColors = (newColors) => {
+    setColors(newColors);
+  };
+
+  const updateFonts = (newFonts) => {
+    setFonts(newFonts);
+  };
+
   return (
-    <ThemeContext.Provider value={[colors, setColors, fonts, setFonts]}>
+    <ThemeContext.Provider value={{ colors, fonts, updateColors, updateFonts }}>
       {children}
     </ThemeContext.Provider>
   );
+};
+
+ThemeProvider.propTypes = {
+  tokens: PropTypes.object.isRequired,
+  children: PropTypes.node.isRequired,
 };
 
 export default ThemeProvider;
