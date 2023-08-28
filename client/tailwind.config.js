cd2b6d1a1d424e5e99aa5dad2b0d67fefc231d0e
criztiandev <criztiandev@gmail.com>
2023-08-28 10:50:42 +0800

    Added Styling Default

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        primary: ["Roboto", "sans-serif"],
+        secondary: ["Roboto Slab", "serif"],
+      },
+
+      colors: {
+        primary: "#1E90FF",
+        secondary: "#FFA500",
+      },
+    },
   },
   plugins: [],
 };
