498c679043ba9a750c557e4ab63734f1fb3e1fe0
criztiandev <criztiandev@gmail.com>
2023-09-12 10:27:13 +0800

    Created a Base layout and fix other issues

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,9 @@
 import Badge from "./components/Badge";
 import Card from "./components/Card";
 import IconButton from "./components/IconButton";
-
-import Nav from "./containers/Nav";
 import Typography from "./components/Typography";
+
+import BaseLayout from "./layouts/BaseLayout";
 
 interface CardDataType {
   added: number;
@@ -12,7 +12,6 @@ interface CardDataType {
 }
 
 const App = () => {
-  const name = "Criztian Jade M Tuplano";
   const cardData: CardDataType[] = [
     {
       added: 23,
@@ -32,67 +31,51 @@ const App = () => {
   ];
 
   return (
-    <>
-      <Nav />
-
-      <main className="px-8 py-4 flex flex-col gap-6">
-        <header className="flex justify-between items-center">
-          <div>
-            <Typography as="h1">Welcome {name}</Typography>
-            <Typography as="small">Hello there, I miss you</Typography>
-          </div>
-
-          <div className="flex gap-4">
-            <IconButton />
-            <IconButton />
-          </div>
-        </header>
-
-        <section className="grid grid-cols-3 gap-4">
-          {cardData.map(items => (
-            <div
-              key={items.title}
-              className="flex flex-col gap-4 px-6 py-4 border border-black rounded-[5px]">
-              <div className="flex justify-between">
-                <Typography as="h5">Total</Typography>
-                <Badge>+{items.added}</Badge>
-              </div>
-
-              <div>
-                <Typography as="h2">{items.total}</Typography>
-                <Typography as="p">{items.title}</Typography>
-              </div>
+    <BaseLayout>
+      <section className="grid grid-cols-3 gap-4">
+        {cardData.map(items => (
+          <div
+            key={items.title}
+            className="flex flex-col gap-4 px-6 py-4 border border-black rounded-[5px]">
+            <div className="flex justify-between">
+              <Typography as="h5">Total</Typography>
+              <Badge>+{items.added}</Badge>
             </div>
-          ))}
-        </section>
 
-        <section className="grid grid-cols-[auto_370px] gap-4">
-          <Card>
-            <Card.Header
-              title=" Applicants Graph "
-              className="flex justify-between items-center">
-              <IconButton />
-            </Card.Header>
-            <Card.Content>
-              <div className="flex  gap-4 p-4">
-                <span className="flex items-center gap-4">
-                  <span className="p-4 bg-blue-400 rounded-full"></span>
-                  <Typography as="p">Junior</Typography>
-                </span>
-                <span className="flex items-center gap-4">
-                  <span className="p-4 bg-blue-500 rounded-full"></span>
-                  <Typography as="p">Senior</Typography>
-                </span>
-              </div>
-            </Card.Content>
-          </Card>
+            <div>
+              <Typography as="h2">{items.total}</Typography>
+              <Typography as="p">{items.title}</Typography>
+            </div>
+          </div>
+        ))}
+      </section>
 
-          <Card>
-            <Card.Header title="Blogs"></Card.Header>
-          </Card>
-        </section>
-      </main>
-    </>
+      <section className="grid grid-cols-[auto_370px] gap-4">
+        <Card>
+          <Card.Header
+            title=" Applicants Graph "
+            className="flex justify-between items-center">
+            <IconButton />
+          </Card.Header>
+          <Card.Content>
+            <div className="flex  gap-4 p-4">
+              <span className="flex items-center gap-4">
+                <span className="p-4 bg-blue-400 rounded-full"></span>
+                <Typography as="p">Junior</Typography>
+              </span>
+              <span className="flex items-center gap-4">
+                <span className="p-4 bg-blue-500 rounded-full"></span>
+                <Typography as="p">Senior</Typography>
+              </span>
+            </div>
+          </Card.Content>
+        </Card>
+
+        <Card>
+          <Card.Header title="Blogs"></Card.Header>
+        </Card>
+      </section>
+    </BaseLayout>
   );
 };
 
--- client/src/components/Table.tsx
+++ client/src/components/Table.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const Table = () => {
   return <div>Table</div>;
 };
