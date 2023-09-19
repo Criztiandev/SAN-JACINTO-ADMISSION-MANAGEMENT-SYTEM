3283ade91e3f3f68b71e4099479959aa43750f27
criztiandev <criztiandev@gmail.com>
2023-09-20 03:51:12 +0800

    Improved Registration Interface

--- client/src/interface/registrationInterface.ts
+++ client/src/interface/registrationInterface.ts
@@ -1,7 +1,10 @@
-export interface PanelContent {
+import { BaseProps } from "./componentInterface";
+
+// Layout Props
+export interface panelTitleInterface {
   "Grade Level"?: string;
   "Student Details"?: string;
-  "Pesonal Details"?: string;
+  "Personal Details"?: string;
   "Permanent Address"?: string;
   "Guardian Details"?: string;
   "Other Details"?: string;
@@ -9,4 +12,9 @@ export interface PanelContent {
   Checkpoint?: string;
   "Application Form"?: string;
   "Thank you"?: string;
+}
+
+// Registration layout props
+export interface registrationLayoutProps extends BaseProps {
+  activePanel: string;
 }
